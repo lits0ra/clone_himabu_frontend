334b94c9340407acb357233c86147ead03e0ec1a
s0ra <s0ra_kanda@icloud.com>
2020-01-05 10:20:14 +0900

    Initial Commit

---
+++ clone_himabu_frontend/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  clone_himabu_frontend
+//
+//  Created by s0ra on 2020/01/05.
+//  Copyright © 2020 s0ra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
